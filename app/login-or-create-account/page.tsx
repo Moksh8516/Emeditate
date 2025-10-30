4ec30bde5f58e9a86a627acb1920bc3fe3bb23fa
moksh8516 <moksh8516@gmail.com>
2025-10-30 16:50:39 +0530

    undo logic

--- app/login-or-create-account/page.tsx
+++ app/login-or-create-account/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
 import { useEffect } from "react";
 import { completeMagicLinkLogin, getIdToken } from "@/lib/firebaseClient";
 import { API_URL } from "@/lib/config";
